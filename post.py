13dcb2cf5ffb6cee365e864b356e541bf8434c2e
JeanPier1 <jeangonza@upeu.edu.pe>
2020-10-06 13:03:24 -0500

    second commit

--- post.py
+++ post.py
@@ -1,11 +1,18 @@
+from datetime import datetime, date
+
+
 class Post:
-    def __init__(self, title, content):
+    def __init__(self, title, content, date):
         self.title = title
         self.content = content
-
+        self.fecha = date
 
     def json(self):
         return {
-            'title' : self.title,
+            'title': self.title,
             'content': self.content,
+            'date': self.fecha
         }
+
+    def __repr__(self):
+        return '{} by {} the {}'.format(self.title, self.content, datetime.strptime(self.fecha, '%Y-%m-%d %H:%M:%S'), 'ok' if(datetime.strptime(self.fecha, '%Y-%m-%d %H:%M:%S') == '%Y-%m-%d %H:%M:%S') else 'error')
--- tests/unit/post_test.py
+++ tests/unit/post_test.py
@@ -1,13 +1,30 @@
 from unittest import TestCase
 from post import Post
 
+
 class PostTest(TestCase):
     def test_create_post(self):
-        p = Post('Test','Test Content')
-        self.assertEqual('Test',p.title)
+        p = Post('Test', 'Test Content', '12-12-12')
+        self.assertEqual('Test', p.title)
         self.assertEqual('Test Content', p.content)
-    
+        self.assertEqual('12-12-12', p.fecha)
+
     def test_json(self):
-        p = Post('Test','Test Content')
-        expected = {'title' : 'Test','content':'Test Content'}
+        p = Post('Test', 'Test Content', '12-12-12')
+        expected = {'title': 'Test',
+                    'content': 'Test Content',
+                    'date': '12-12-12'}
         self.assertDictEqual(expected, p.json())
+
+    def test_repor(self):
+        p = Post('Test', 'Test Content', '2012-12-12 00:00:00')
+        p2 = Post('Pruebita', 'Contenido dicernido', '2013-12-13 00:00:00')
+        self.assertEqual(
+            p.__repr__(), 'Test by Test Content the 2012-12-12 00:00:00')
+        self.assertEqual(
+            p2.__repr__(), 'Pruebita by Contenido dicernido the 2013-12-13 00:00:00')
+
+    def test_verificar(self):
+        p = Post('Test', 'Test Content', '2013-12-13 00:00:00')
+        self.assertEqual(
+            p.__repr__(), 'Test by Test Content the 2013-12-13 00:00:00')
